76b0a7541ae3a8c611655fb8ce9167f673c55224
dennyberrios <denny_berrios@hotmail.com>
2022-07-02 15:08:56 -0300

    Atualização

--- Front-end/js/cadastro.js
+++
@@ -1,3 +0,0 @@
-function inicio(){
-    location.replace("http://192.168.0.110:5500/Front-end/html/index.html?","self")
-}
---
+++ Front-end/js/jogos.js
@@ -0,0 +1,7 @@
+
+
+function btn1(){document.getElementById("resultado").innerHTML= "Resposta errada";}
+
+function btn2(){document.getElementById("resultado").innerHTML= "Resposta errada";}
+
+function btn3(){document.getElementById("resultado").innerHTML= "Resposta correta";}
